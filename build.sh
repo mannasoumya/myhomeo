31fb094e041975ff13281035118f5572dbae3e55
Manna Soumya <sm323265@gmail.com>
2022-12-05 12:06:42 +0530

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 set -xe
-python3 -m venv venv
+python3.9 -m venv venv
 . ./venv/bin/activate
 pip install pip-tools
 pip-compile -o requirements.txt requirements.in
-pip install -r requirements.txt
+pip-sync
